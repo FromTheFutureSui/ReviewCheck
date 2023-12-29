4a2e1d55aeebc7db80c30762ba6cd07c099e480e
suixinran <suixinran@lucidata.cn>
2023-12-29 11:34:07 +0800

    ADD sort

--- check/sort.py
+++ check/sort.py
@@ -24,3 +24,8 @@ def merge_sort(arr):
             arr[k] = left_half[i]
             i += 1
             k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
